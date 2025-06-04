879746bb968d50c0264e00541c938e6accb46d36
Max Rohde <1448524+mxro@users.noreply.github.com>
2025-06-04 13:20:24 +1000

    Improving converting to H.264

--- convert/to_h264/convert_to_h264.bat
+++ convert/to_h264/convert_to_h264.bat
@@ -2,7 +2,11 @@
 setlocal enabledelayedexpansion
 
 REM Set ffmpeg path
-set FFMPEG_PATH=C:\Users\Max\Videos\ffmpeg-7.1\bin\ffmpeg.exe
+:: Path to ffmpeg executable
+set "BATCH_DIR=%~dp0"
+:: Go up two directories from the batch file location to find the ffmpeg folder
+for %%I in ("%BATCH_DIR%..\..") do set "ROOT_DIR=%%~fI\"
+set "FFMPEG_PATH=%ROOT_DIR%ffmpeg\bin\ffmpeg.exe"
 
 REM Process only original MP4 and MKV files (excluding already processed ones)
 for %%F in (*.mp4 *.mkv) do (
@@ -12,7 +16,7 @@ for %%F in (*.mp4 *.mkv) do (
         set "OUTPUT=%%~nF.h264%%~xF"
 
         echo Processing "!INPUT!" -> "!OUTPUT!"
-        "%FFMPEG_PATH%" -y -i "!INPUT!" -c:v libx264 -preset slow -crf 23 -c:a copy "!OUTPUT!"
+        "%FFMPEG_PATH%" -y -i "!INPUT!" -c:v libx264 -pix_fmt yuv420p -preset slow -crf 23 -c:a copy "!OUTPUT!"
     )
 )
 
